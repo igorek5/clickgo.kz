febc37880c62302519079979b1b3c748814c3565
igorek5 <igorbautin101@gmail.com>
2021-11-22 16:33:38 +0300

    Меню, promo

--- source/js/modules/main-nav.js
+++ source/js/modules/main-nav.js
@@ -1,5 +1,22 @@
 const mainNav = document.querySelector(".main-nav");
 const mainNavButton = document.querySelector(".main-nav__toggle");
+const mainNavLink = document.querySelectorAll(".main-nav__link");
+const body = document.querySelector(".page-body");
+
+const isEscEvent = (evt) => evt.key === "Escape" || evt.key === "Esc";
+
+const linkMenuClick = () => {
+  mainNav.classList.remove("open");
+}
+
+const documentKeydownHandler = (evt) => {
+  if (isEscEvent(evt)) {
+      evt.preventDefault();
+      mainNav.classList.remove("open");
+      closeMenuHandler()
+      mainNavButton.focus();
+  }
+}
 
 const setControlMenu = () => {
   if (mainNav) {
@@ -8,8 +25,23 @@ const setControlMenu = () => {
     mainNavButton.addEventListener("click", function (evt) {
       evt.preventDefault();
       mainNav.classList.toggle("open");
+
+
+      if (mainNav.classList.contains("open")) {
+        document.addEventListener("keydown", documentKeydownHandler);
+        mainNavLink.forEach((el) => {
+          el.addEventListener("click", linkMenuClick);
+        });
+      }
     });
   }
+}
+
+function closeMenuHandler() {
+  document.removeEventListener('keydown', documentKeydownHandler);
+  mainNavLink.forEach((el) => {
+    el.removeEventListener("click", linkMenuClick);
+  })
 }
 
 export { setControlMenu };
